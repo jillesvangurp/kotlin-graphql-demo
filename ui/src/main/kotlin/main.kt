dbf8d1609ddbeb9340deee24e533d57853dc34e9
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2020-01-22 14:47:00 +0100

    clean code and a count down thingy

--- ui/src/main/kotlin/main.kt
+++ ui/src/main/kotlin/main.kt
@@ -1,50 +1,48 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.await
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 import kotlinx.html.dom.create
 import kotlinx.html.h1
+import kotlinx.html.p
 import kotlinx.html.pre
 import org.w3c.dom.HTMLDivElement
 import org.w3c.fetch.RequestInit
 import kotlin.browser.document
 import kotlin.browser.window
-import kotlin.dom.appendText
 import kotlin.js.json
 
 
 suspend fun main() {
 
     val rootEl = document.getElementById("root_el") as HTMLDivElement
-    val gqlRequest = document.create.pre { +"-" }
-    val gqlResponse = document.create.pre { +"-" }
+    document.create.apply {
+        val gqlRequest = pre { +"-" }
+        val gqlResponse = pre { +"-" }
 
-    // fugly UI
-    rootEl.append(
-        document.create.h1 { +"Graphql Request" },
-        gqlRequest,
-        document.create.h1 { +"Graphql Response" },
-        gqlResponse,
-        document.create.h1 { +"Leftpad demo" },
-        document.createElement("p").appendText("I can has leftPad in kotlin '${leftPad("Foo", 50, "*")}'")
-    )
+        // fugly UI
+        rootEl.append(
+            h1 { +"Graphql Request" },
+            gqlRequest,
+            h1 { +"Graphql Response" },
+            gqlResponse,
+            h1 { +"Leftpad demo" },
+            p { +"I can has leftPad in kotlin '${leftPad("Foo", 50, "*")}'" }
+        )
 
-    // print to the console!
-    val requestJson = JSON.stringify(json().apply {
-        this["operationName"] = null
-        this["variables"] = json()
-        this["query"] = "{ coders { name }}"
-    }, null, 2)
+        // print to the console!
+        val requestJson = JSON.stringify(json().apply {
+            this["operationName"] = null
+            this["variables"] = json()
+            this["query"] = "{ coders { name }}"
+        }, null, 2)
 
-    println(requestJson)
-    gqlRequest.textContent = requestJson
+        println(requestJson)
+        gqlRequest.textContent = requestJson
 
-    val req = RequestInit(method = "POST", headers = json().apply {
-        this["Content-Type"] = "application/json"
-    }, body = requestJson)
+        val req = RequestInit(method = "POST", headers = json().apply {
+            this["Content-Type"] = "application/json"
+        }, body = requestJson)
 
 
 // without co-routines
@@ -57,13 +55,19 @@ suspend fun main() {
 //        }
 //    }
 
-    // lets use co-routines
-    // we marked main as suspend so we have a coroutine scope
-    coroutineScope {
-        async {
-            val text = fetchGraphql(req)
-            console.log(text)
-            gqlResponse.textContent = JSON.stringify(JSON.parse(text), null, 2)
+        // lets use co-routines
+        // we marked main as suspend so we have a coroutine scope
+        coroutineScope {
+            async {
+                // artificially delay
+                (5 downTo 1).forEach {
+                    gqlResponse.textContent = "in $it ..."
+                    delay(1000)
+                }
+                val text = fetchGraphql(req)
+                console.log(text)
+                gqlResponse.textContent = JSON.stringify(JSON.parse(text), null, 2)
+            }
         }
     }
 
@@ -73,6 +77,7 @@ private suspend fun fetchGraphql(req: RequestInit): String = try {
     val response = window.fetch("http://localhost:8081/graphql", req).await()
     response.text().await()
 } catch (e: Exception) {
+    // technically not needed but lets catch it here
     println("OOPSIE ${e.message}")
     "${e.message}"
 }

70f32c51e628c3a60fda5ce124768533caff098c
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2020-01-24 16:36:59 +0100

    add some broken config for the google closure compiler

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 apply {
+    // this plugin minifies the kotlin js output. Several bugs currently with this (e.g. multiple npms having an index.js)
     plugin("kotlin-dce-js")
 }
 
@@ -13,9 +14,13 @@ repositories {
     jcenter()
 }
 
+val closureCompiler by configurations.creating
+
 dependencies {
     implementation(kotlin("stdlib-js"))
     testImplementation(kotlin("test-js"))
+    // needed for the google closure compiler, which is recommended to minify the output of dce
+    closureCompiler("com.google.javascript:closure-compiler:v20200112")
 }
 
 kotlin {
@@ -34,5 +39,40 @@ kotlin {
 
             }
         }
+    }
+}
+
+tasks {
+    register<Copy>("copy-index-html") {
+        from("src/main/resources/index.html")
+        into("build/packaged")
+    }
+    // does not work because of module errors
+    register<JavaExec>("googleClosureCompileDceJs") {
+//        dependsOn("copy-index-html","assemble")
+        classpath = closureCompiler
+        main = "com.google.javascript.jscomp.CommandLineRunner"
+        args = listOf(
+            "--compilation_level=SIMPLE_OPTIMIZATIONS",
+//            "--process_common_js_modules", // errors with a  ERROR - [JSC_INVALID_MODULE_PATH] Invalid module path "kotlin" for resolution mode "BROWSER"
+            // nope, this doesn't work either
+            "--js_output_file=build/packaged/${rootProject.name}.js",
+            "build/kotlin-js-min/main/kotlin.js",
+            "build/kotlin-js-min/main/kotlinx-coroutines-core.js",
+            "build/kotlin-js-min/main/index.js", // yes this is a problem if you have more than 1 npm using that as their main js file, sigh
+            "build/kotlin-js-min/main/${rootProject.name}.js"
+        )
+    }
+
+    // processes the output in distributions (12M) and produces a similarly sized file that does not work (unlike the original)
+    register<JavaExec>("googleClosureCompileDistributionJs") {
+        dependsOn("copy-index-html","assemble")
+        classpath = closureCompiler
+        main = "com.google.javascript.jscomp.CommandLineRunner"
+        args = listOf(
+            "--compilation_level=SIMPLE_OPTIMIZATIONS",
+            "--js_output_file=build/packaged/${rootProject.name}.js",
+            "build/distributions/${rootProject.name}.js"
+        )
     }
 }
